7cd8f283b008ca82480b6d4a950b44c45cf8e133
Arthuro Cueva <arthuro.cueva@gmail.com>
2021-04-28 18:34:04 -0500

    Add app icon, update credentials and version library

--- app/src/main/java/com/cueva/movieapprt/application/presentation/ui/movies/list/MoviesViewModel.kt
+++ app/src/main/java/com/cueva/movieapprt/application/presentation/ui/movies/list/MoviesViewModel.kt
@@ -3,7 +3,9 @@ package com.cueva.movieapprt.application.presentation.ui.movies.list
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.paging.*
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import com.cueva.movieapprt.application.framework.MovieDataSource
 import com.cueva.movieapprt.application.presentation.entity.MovieApp
 import javax.inject.Inject
